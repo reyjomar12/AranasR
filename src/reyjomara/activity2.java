904eae95dee3d14062809f644976d16ec11ae4dd
Rejomar <reyjomar188@gmail.com>
2025-08-17 18:44:33 +0800

    Update activity2.java
    Updated act 2

--- src/reyjomara/activity2.java
+++ src/reyjomara/activity2.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package reyjomara;
 
-/**
- *
- * @author PC6
- */
 public class activity2 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scan.nextInt();
+        
+        if(num == 0) {
+            System.out.println("Inputted number is Zero"); 
+        }else if(num%2 == 0) {
+            System.out.println("Inputted number is even");
+        }else{
+            System.out.println("Inputted number is odd");
+        }
+    }
 }
